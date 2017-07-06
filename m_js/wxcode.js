254a745fd7d3393ddc8b0999e5cb246221de2698
rmtuyuan <mr_cui1004@163.com>
2017-07-06 17:34:31 +0800

    zushouyou
    updata all project

---
+++ m_js/wxcode.js
@@ -0,0 +1,5 @@
+//微信开发
+//APPid :wxaf994b737f431650
+//appsecret : 3de5bcd4ed72eeebddd68a3e2f6247e1
+
+//https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
